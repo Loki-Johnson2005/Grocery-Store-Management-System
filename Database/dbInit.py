5e0d313ac9f2e2b6e38ba117d974ffda0beaf621
Lukong Jonathan <129514461+Loki-Johnson2005@users.noreply.github.com>
2024-06-12 19:50:45 +0100

    Delete Database directory

--- Database/dbInit.py
+++
@@ -1,9 +0,0 @@
-from Database.database import Base, engine
-from model import user
-from model import source
-from model import complaint
-
-
-def create_tables():
-    Base.metadata.create_all(engine)
-
